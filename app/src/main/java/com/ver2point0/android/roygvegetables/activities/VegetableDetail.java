20b967e8dd60b1d3476c4d39edf02d8a808f8d71
John McIntosh II <jmci10@hotmail.com>
2017-07-08 00:57:34 -0400

    update android manifest

--- app/src/main/java/com/ver2point0/android/roygvegetables/activities/VegetableDetail.java
+++
@@ -1,6 +0,0 @@
-package com.ver2point0.android.roygvegetables.activities;
-
-
-
-public class VegetableDetail {
-}
---
+++ app/src/main/java/com/ver2point0/android/roygvegetables/activities/VegetableDetailActivity.java
@@ -0,0 +1,7 @@
+package com.ver2point0.android.roygvegetables.activities;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+public class VegetableDetailActivity extends AppCompatActivity {
+}
