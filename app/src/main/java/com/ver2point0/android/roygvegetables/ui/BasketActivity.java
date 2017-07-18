87c4226952f9dbda00a9680380dfbb5380c3901d
John McIntosh II <jmci10@hotmail.com>
2017-07-17 20:06:34 -0400

    add fab button code

--- app/src/main/java/com/ver2point0/android/roygvegetables/ui/BasketActivity.java
+++ app/src/main/java/com/ver2point0/android/roygvegetables/ui/BasketActivity.java
@@ -1,7 +1,10 @@
 package com.ver2point0.android.roygvegetables.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.ver2point0.android.roygvegetables.R;
 
@@ -11,5 +14,14 @@ public class BasketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fabIntent = new Intent(BasketActivity.this, EditorActivity.class);
+                startActivity(fabIntent);
+            }
+        });
     }
 }
